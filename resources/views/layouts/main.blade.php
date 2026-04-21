29f1a0a731250932cb6f68346e93bcd4c2761202
FarhanNulAziz <farhannulaziz160@gmail.com>
2026-04-21 13:49:20 +0700

    Startting project

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SIPRO - Sistem Produk </title>
+<meta name="author" content="Farhan Nul Aziz">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+@yield('content')
+</div>
+</body>
+</html>
